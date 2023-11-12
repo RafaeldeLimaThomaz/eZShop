35b9fec47f1d812f8e4819eecf059ef00636cc9f
Rafael de Lima Thomaz <79thomaz@gmail.com>
2023-11-12 11:53:12 -0300

    stripe integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,26 @@ import {MatTableModule} from '@angular/material/table';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CartService } from './services/cart.service';
+import { StoreService } from './services/store.service';
+import { HttpClientModule } from '@angular/common/http';
     
     
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    ProductBoxComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
@@ -42,8 +55,9 @@ import { HeaderComponent } from './components/header/header.component';
     MatTableModule,
     MatBadgeModule,
     MatSnackBarModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CartService, StoreService], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
